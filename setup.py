2e999dff4c8b3b45fe6592023c0c0d805fcfc347
Floris Van der krieken <floris@internal.florisvdk.net>
2019-10-10 15:40:37 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'unifiled',
   packages = ['unifiled'],
-  version = '0.6',
+  version = '0.7',
   license='MIT',
   description = 'Easily connect to Ubiquiti Unifi led devices',
   author = 'Floris Van der krieken',
